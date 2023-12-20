b9b07ed2e23cc439269207d1a32e5bc44a3e3968
Vincent <117008605+Colantovincent@users.noreply.github.com>
2023-12-20 17:57:19 +0100

    DB
    Added a basic DB to clean the code, it now features a bunch of people. Better markup and script. Redundancy is now handled in the code (if you answer "yes" to a question it won't give you question you are obliged to say "no" to)

--- app.js
+++ app.js
@@ -2,77 +2,161 @@ const si = document.getElementById("si");
 const no = document.getElementById("no");
 const domanda = document.getElementById("domanda");
 const risposta = document.getElementById("risposta");
-const persone = [
-    {
-        nome: "Leonardo da Vinci",
-        nascita: 1472,
-        sesso: true,
-        vivo: false,
-        ruolo: "scienziato"
-    },
-    {
-        nome: "Michael Jackson",
-        nascita: 1960,
-        sesso: true,
-        vivo: false,
-        ruolo: "musicista"
-    },
-    {
-        nome: "Frida Khalo",
-        nascita: 1907,
-        sesso: false,
-        vivo: false,
-        ruolo: "artista"
-    },
-    {
-        nome: "Samantha Cristoforetti",
-        nascita: 1977,
-        sesso: false,
-        vivo: true,
-        ruolo: "scienziato"
-    }
-];
+const restart = document.getElementById("restart");
+const happy = document.querySelector(".img-thumbnail");
 const domande = [
     {
-        text: "Il tuo personaggio è vivo?",
+        text: "è vivo?",
         vivo: true
     },
     {
-        text: "Il tuo personaggio è uomo?",
+        text: "è uomo?",
         sesso: true
     },
     {
-        text: "Il tuo personaggio è uno scienziato?",
-        ruolo: "scienziato"
+        text: "è uno scienziato?",
+        ruolo: "Scienziato"
     },
     {
-        text: "Il tuo personaggio è un artista?",
-        ruolo: "artista"
+        text: "è un pittore o scultore?",
+        ruolo: "Artista"
     },
     {
-        text: "Il tuo personaggio è un musicista?",
-        ruolo: "musicista"
+        text: "è un musicista, cantante o compositore?",
+        ruolo: "Musicista"
+    },
+    {
+        text: "è un ingegnere?",
+        ruolo: "Ingegnere"
+    },
+    {
+        text: "è uno scrittore/scenografo?",
+        ruolo: "Scrittore"
+    },
+    {
+        text: "è un imprenditore?",
+        ruolo: "Imprenditore"
+    },
+    {
+        text: "è un programmatore?",
+        ruolo: "Programmatore"
+    },
+    {
+        text: "è un matematico?",
+        ruolo: "Matematico"
+    },
+    {
+        text: "è un poeta?",
+        ruolo: "Poeta"
+    },
+    {
+        text: "è di epoca contemporanea?",
+        epoca: "contemporanea"
+    },
+    {
+        text: "è di epoca rinascimentale?",
+        epoca: "rinascimentale"
+    },
+    {
+        text: "è di epoca medioevale?",
+        epoca: "medioevo"
+    },
+    {
+        text: "è vissuto/nato nel secolo 1800?",
+        epoca: "restaurazione"
+    },
+    {
+        text: "è nato nel periodo A.C.?",
+        epoca: "avanti cristo"
+    },
+    {
+        text: "è nato in America?",
+        nazione: "America"
+    },
+    {
+        text: "è Danese?",
+        nazione: "Danimarca"
+    },
+    {
+        text: "è Ceco?",
+        nazione: "Repubblica Ceca"
+    },
+    {
+        text: "è Italiano?",
+        nazione: "Italia"
+    },
+    {
+        text: "è spagnolo?",
+        nazione: "Spagna"
+    },
+    {
+        text: "è giapponese?",
+        nazione: "Giappone"
+    },
+    {
+        text: "è francese?",
+        nazione: "Francia"
+    },
+    {
+        text: "è inglese?",
+        nazione: "Inghilterra"
     }
 ];
-let i, parametro, persona = [...persone], domandePossibili = [...domande];
-const cambiaDomanda = () => {
-    i = Math.floor(Math.random() * (domandePossibili.length - 1));
-    domanda.textContent = domandePossibili[i].text;
-    parametro = Object.keys(domandePossibili[i])[1];
+let persone;
+async function fetching() {
+    try {
+        fetch("data.json").then(response => response.json()).then(data =>{persone = data; setEvent()});
+    } catch (error) {
+        console.error("Errore: " + error);
+    }
+}
+fetching();
+const setEvent = () => {
+    let i, parametro, persona = [...persone], domandePossibili = [...domande];
+    const cambiaDomanda = () => {
+        console.log(persona);
+        i = Math.floor(Math.random() * (domandePossibili.length - 1));
+        domanda.textContent = "Il tuo personaggio " + domandePossibili[i].text;
+        parametro = Object.keys(domandePossibili[i])[1];
+    };
+    cambiaDomanda();
+    const endProgram = () => {
+        if (persona.length >= 2) {
+            for (let el in persona) {
+                risposta.textContent += el.nome + "\n";
+            }
+            happy.src = "/resources/cat.gif";
+            happy.alt = "Happy Happy Happy";
+        } else if (persona.length === 0) {
+            risposta.textContent = "Wo-ho something went wrong~";
+            happy.src = "/resources/fyou.png";
+            happy.alt = "F* you";
+        } else {
+            risposta.textContent = persona[0].nome; 
+            happy.src = "/resources/cat.gif";
+            happy.alt = "Happy Happy Happy";
+        }
+        si.disabled = true;
+        no.disabled = true;
+        happy.hidden = false;
+    };
+    si.addEventListener("click", () => {
+        persona = persona.filter((el) => el[parametro] === domandePossibili[i][parametro]);
+        domandePossibili.splice(i, 1);
+        domandePossibili = domandePossibili.filter(el => Object.keys(el)[1] != parametro); //filtra l'array di domande per togliere le ridondanze
+        (persona.length < 2 || domandePossibili.length == 0) ? endProgram() : cambiaDomanda();
+    });
+    no.addEventListener("click", () => {
+        persona = persona.filter((el) => el[parametro] != domandePossibili[i][parametro]);
+        domandePossibili.splice(i, 1);
+        (persona.length < 2 || domandePossibili.length == 0) ? endProgram() : cambiaDomanda();
+    });
+    restart.addEventListener("click", () => {
+        si.disabled = false;
+        no.disabled = false;
+        happy.hidden = true;
+        risposta.textContent = "";
+        persona = [...persone];
+        domandePossibili = [...domande];
+    });
 };
-cambiaDomanda();
-const endProgram = () => {
-    risposta.textContent =  "All this time... You've been thinking of... " + persona[0].nome;
-    si.disabled = true;
-    no.disabled = true;
-};
-si.addEventListener("click", () => {
-    persona = persona.filter((el) => el[parametro] === domandePossibili[i][parametro]);
-    domandePossibili.splice(i, 1);
-    persona.length == 1 ? endProgram() : cambiaDomanda();
-});
-no.addEventListener("click", () => {
-    persona = persona.filter((el) => el[parametro] != domandePossibili[i][parametro]);
-    domandePossibili.splice(i, 1);
-    persona.length == 1 ? endProgram() : cambiaDomanda();
-});
