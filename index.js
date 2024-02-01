b794fd27d079f9aa00f75ffc483499d62dc1f4ff
Jhosef8455 <132928126+Jhosef8455@users.noreply.github.com>
2024-02-01 11:56:52 +0100

    FINISHED PROJECT
    WE GONNA GO TO REACTJS WITH THIS ONE!!! :face_silouhette:

---
+++ index.js
@@ -0,0 +1,31 @@
+import persone from './data.json' with {type: 'json'};
+const listaPers1 = document.getElementById("listaPers1");
+const listaPers2 = document.getElementById("listaPers2");
+let nomi = [];
+function bananaSplit(gelato, cherryOnTop) {
+    let cono = [];
+    const mid = Math.floor(gelato.length / 2);
+    for (let persona of gelato) {
+        cono.push(persona.nome);
+        if (cono.length === mid && cherryOnTop) {
+            break;
+        }
+    }
+    return cono;
+}
+nomi = bananaSplit(persone, true);
+let temp = persone.length / 2;
+for (let i = 0; i < temp; i++) {
+    persone.splice(0, 1);
+}
+ console.log(persone);
+function elaboraNomi(elaborabili) {
+    let lista = "";
+    for (let nome of elaborabili) {
+        lista += "<li>" + nome + "</li>\n";
+    }
+    return lista;
+}
+listaPers1.innerHTML = elaboraNomi(nomi);
+nomi = bananaSplit(persone, false);
+listaPers2.innerHTML = elaboraNomi(nomi);
